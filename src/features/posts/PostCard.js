c6fffcd4b9f85ab3e97ed1d5cc7fc9ee516955d6
Membia <info@membia.de>
2023-02-23 14:11:35 +0100

    Reduced border width

--- src/features/posts/PostCard.js
+++ src/features/posts/PostCard.js
@@ -5,7 +5,7 @@ import { faComment } from "@fortawesome/free-solid-svg-icons";
 export default function PostCard() {
   return (
     <div className="flex flex-col drop-shadow-[0_20px_20px_rgba(0,0,0,0.07)] border-2 p-4 sm:p-8 rounded-xl bg-white border-gray-100">
-      <div className="border-b-2 pb-8 border-gray-100 mb-6">
+      <div className="border-b-1 pb-8 border-gray-100 mb-6">
         <h2 className="font-bold text-lg text-left mb-4">This is a title of a reddit post</h2>
         <img
           src="https://images.unsplash.com/photo-1677108581323-7050fbfd528f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
